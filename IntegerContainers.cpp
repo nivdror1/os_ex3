eebe2cff7b69edb7a72cda74b1aeb23c63cd9a2d
nivdror1 <nivdror1@gmail.com>
2017-05-04 17:14:09 +0300

    written integerContainers

--- IntegerContainers.cpp
+++ IntegerContainers.cpp
@@ -1,5 +1,34 @@
-//
-// Created by nivdror1 on 5/4/17.
-//
+
 
 #include "IntegerContainers.h"
+
+
+/**
+ * a constructor
+ * @param newValue the value
+ * @return an instance of the class
+ */
+IntegerContainers::IntegerContainers(int newValue):
+		value(newValue){};
+
+/**
+ * d-tor
+ */
+~IntegerContainers(){
+	this->value=0;
+}
+
+/**
+ * get the value
+ * @return return the value
+ */
+int IntegerContainers::getValue() const{
+	return this->value;
+}
+
+/**
+ * set the value
+ */
+void IntegerContainers::setValue(int newVal){
+	this->value=newVal;
+}
