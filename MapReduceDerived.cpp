806ce41bc0c00c64b71949dd3ea832dfe382bc16
IdoShachar <ido.shachar@mail.huji.ac.il>
2017-05-04 17:35:24 +0300

    shit

---
+++ MapReduceDerived.cpp
@@ -0,0 +1,19 @@
+//
+// Created by ido.shachar on 5/4/17.
+//
+
+#include "MapReduceDerived.h"
+
+MapReduceDerived::MapReduceDerived()
+{
+}
+
+void MapReduceBase::Map(const k1Base *const key, const v1Base *const val) const
+{
+
+}
+
+void MapReduceBase::Reduce(const k2Base *const key, const V2_VEC &vals) const
+{
+
+}
---
+++ MapReduceDerived.h
@@ -0,0 +1,16 @@
+//
+// Created by ido.shachar on 5/4/17.
+//
+
+#ifndef OS_EX3_MAPREDUCEDERIVED_H
+#define OS_EX3_MAPREDUCEDERIVED_H
+
+#include "MapReduceFramework.h"
+
+class MapReduceDerived: public MapReduceBase {
+
+public:
+    MapReduceDerived();
+};
+
+#endif //OS_EX3_MAPREDUCEDERIVED_H
