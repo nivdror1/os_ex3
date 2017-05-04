0f5f28af22a6d5ec57a38a2debe9fc2885abeb71
nivdror1 <nivdror1@gmail.com>
2017-05-04 19:13:15 +0300

    written integerContainers

--- MapReduceDerived.cpp
+++ MapReduceDerived.cpp
@@ -10,7 +10,17 @@ MapReduceDerived::MapReduceDerived()
 
 void MapReduceBase::Map(const k1Base *const key, const v1Base *const val) const
 {
+	StringContainers * strKey= (StringContainers *)key;
+	StringContainers * strVal= (StringContainers *)val;
 
+	fs::directory_iterator end_iter;
+	for(fs::directory_iterator iter(strVal->getData());
+	    iter!=end_iter;++iter){
+		std::string fileName = iter->path().filename().string();
+		if(fileName.find(strKey->getData())){
+			Emit2(new StringContainers(fileName), new IntegerContainers(1));
+		}
+	}
 }
 
 void MapReduceBase::Reduce(const k2Base *const key, const V2_VEC &vals) const
