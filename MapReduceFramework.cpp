a6d795c8976240950b054d8c03ea5db2557c5cf0
nivdror1 <nivdror1@gmail.com>
2017-05-05 16:27:11 +0300

    init finished for now

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -11,28 +11,28 @@ typedef std::vector<Map_Vec*> PTC;
 std::vector<ExecMap*> execMapVector;
 Shuffle * shuffle;
 PTC pthreadToContainer;
+pthread_mutex_t pthreadToContainerMutex;
 
 /**
- * initate the threads of the mapping and shuffling, and also initate the
+ * initiate the threads of the mapping and shuffling, and also initiate the
  * pthreadToContainer
  * @param numThread the number of threads to be create while mapping
- * @param mapReduce an object
- * @param itemsVec
+ * @param mapReduce an object that contain the map function
  */
-void init(int numThread,MapReduceBase& mapReduce,IN_ITEMS_VEC& itemsVec){
-
-	pthread_mutex_t pthreadToContainerMutex;
+void init(int numThread,MapReduceBase& mapReduce){
+	//lock the pthreadToContainer
 	pthread_mutex_lock(&pthreadToContainerMutex);
+	//spawn the new threads
 	for(int i=0;i<numThread;i++){
 		execMapVector[i] =  new ExecMap(i,mapReduce.Map);
  	}
-
 	shuffle= new Shuffle();
 
+	//initiate the vector pthreadToContainer
 	for(int j=0;j<numThread;j++){
 		pthreadToContainer[j]=execMapVector[j]->getPastMapVector();
 	}
-
+	//unlock the pthreadToContainer
 	pthread_mutex_unlock(&pthreadToContainerMutex);
 
 
@@ -41,4 +41,5 @@ void init(int numThread,MapReduceBase& mapReduce,IN_ITEMS_VEC& itemsVec){
 OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase& mapReduce, IN_ITEMS_VEC& itemsVec,
                                     int multiThreadLevel, bool autoDeleteV2K2){
 
+	init(multiThreadLevel,mapReduce);
 }
