df2adca98b87ac886066c95bccca49abfb6d9776
nivdror1 <nivdror1@gmail.com>
2017-05-08 15:57:02 +0300

    struct Resources created

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -10,7 +10,27 @@ typedef std::vector<std::pair<pthread_t,Map_Vec*>> PTC;
 std::vector<ExecMap*> execMapVector;
 Shuffle * shuffle;
 PTC pthreadToContainer;
-pthread_mutex_t pthreadToContainerMutex;
+
+/**
+ * a struct of resources for the ExecMap objects
+ */
+struct Resources{
+	/** a mutex on the pthreadToCotnainer*/
+	pthread_mutex_t pthreadToContainerMutex;
+
+	/** a mutex on the inputVectorIndexMutex*/
+	pthread_mutex_t inputVectorIndexMutex;
+
+	/** the input vector that was given in the runMapReduceFramework*/
+	IN_ITEMS_VEC inputVector;
+
+	/** the index of current location in the input vector*/
+	int inputVectorIndex=0;
+
+	/** an object of mapReduce which contain the map function*/
+	MapReduceBase mapReduce;
+
+}resources;
 
 /**
  * initiate the threads of the mapping and shuffling, and also initiate the
@@ -18,15 +38,18 @@ pthread_mutex_t pthreadToContainerMutex;
  * @param numThread the number of threads to be create while mapping
  * @param mapReduce an object that contain the map function
  */
-void init(int numThread,MapReduceBase& mapReduce){
+void init(int numThread,MapReduceBase& mapReduceBase,IN_ITEMS_VEC& itemsVec){
+	resources.inputVector=itemsVec;
+	resources.mapReduce=mapReduceBase;
+
 	//lock the pthreadToContainer
-	pthread_mutex_lock(&pthreadToContainerMutex);
+	pthread_mutex_lock(&resources.pthreadToContainerMutex);
 	//spawn the new threads and initiate the vector pthreadToContainer
 	for(int i=0;i<numThread;i++){
         // maybe give up on execMapVector and make pTC vector of <thread_self, ExecMap*>
-		execMapVector[i] =  new ExecMap(i,&mapReduce);
+		execMapVector[i] =  new ExecMap(&mapReduce,itemsVec,inputVectorIndex);
 		pthreadToContainer.push_back(std::make_pair(execMapVector[i]->getSelf()
-                                                       ,execMapVector[i]->getPastMapVector()));
+                                                       ,new ExecMap(&mapReduce,itemsVec,inputVectorIndex)));
  	}
 	shuffle= new Shuffle();
 
